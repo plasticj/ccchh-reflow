cac77d99b06f14f39e166b236865fbcfecf1bbbe
plasticj <tortenstueck3@web.de>
2011-07-23 00:02:39 +0200

    stand .. end... createfilehandler nutzen?

--- editor.py
+++ editor.py
@@ -1,33 +1,10 @@
 #!/usr/bin/env python3
 
 from tkinter import *
+from graph import *
 
-class Graph(Canvas):
-	def __init__(self, parent= None, **kw):
-		Canvas.__init__(self,parent, kw)
-		self.create_rectangle(10,10,30,30,fill="green")
-		self.rangex=(0,1)
-		self.rangey=(0,1)
-
-	def drawaxis(self):
-		starty = self.winfo_reqheight()-20
-		startx = 20
-		endy   = 20
-		endx   = self.winfo_reqwidth()-20
-
-		#y axis
-		self.create_line(startx, starty, startx, endy)
-		#x axis
-		self.create_line(startx, starty, endx, starty)
-
-		xpix = self.winfo_reqwidth() -40
-		xr = self.rangex[1]-self.rangex[0]
-		xtnum = int(xr / self.ticksx)
-		w = xpix / xtnum
-		for i in range(xtnum):
-			x = startx + i*w
-			self.create_line(x, starty+2, x,starty-2 )
-
+import re
+import serial
 
 class App:
 	def __init__(self, master):
@@ -43,12 +20,15 @@ class App:
 
 		self.gw = 800
 		self.gh = 600
+
 		self.graph = Graph(master, width=self.gw, height=self.gh, background="white")
-		self.graph.rangex = (10,100)
-		self.graph.rangey = (10,100)
-		self.graph.ticksy = 20
-		self.graph.ticksx = 20
-		self.graph.drawaxis()
+		self.graph.rangex = (0,10)
+		self.graph.rangey = (10,150)
+		self.graph.xtics = 10
+		self.graph.ytics = 50
+		self.graph.xlabel="t"
+		self.graph.ylabel="T"
+		self.graph.draw()
 
 #		self.graph.create_line(10,10,400,400, fill="red")
 		self.graph.pack()
@@ -71,17 +51,18 @@ class App:
 				self.temp_to_y(temp)+2,
 				fill="red")
 
-	def draw(self, t, temp):
-		self.mkpoint(t,temp)
-		self.graph.create_line(
-				self.t_to_x(self.lastpoint[0]),
-				self.temp_to_y(self.lastpoint[1]),
-				self.t_to_x(t),
-				self.temp_to_y(temp),
-
-				fill="blue",
-				width=2)
-
+	def add_value(self, t, temp):
+		self.graph.add_point(t,temp)
+#		self.graph.create_line(
+#				self.t_to_x(self.lastpoint[0]),
+#				self.temp_to_y(self.lastpoint[1]),
+#				self.t_to_x(t),
+#				self.temp_to_y(temp),
+#
+#				fill="blue",
+#				width=2)
+#
+		self.graph.draw()
 		self.lastpoint=(t,temp)
 
 
@@ -89,19 +70,54 @@ class App:
 	def say_hi(self):
 		print("hi there, everyone!")
 
+
+
 root = Tk()
-
 app = App(root)
+t = 0
+def handle_serial():
+	global t
+	Temp = None
+	try:
+		ser = serial.Serial('/dev/ttyUSB0',1000000,timeout=2,parity=serial.PARITY_NONE)
+		line = ser.readline()
+		ser.close()
 
 
-f = open("test.dat")
-i = 1
-for line in f.readlines():
-	x = i
-	y = round(float(line))
-	app.draw(i, y)
-	i = i + 1
+		print(line)
+		if len(line) > 1:
+			Temp = float(re.search('T: (.*?) ', str(line)).group(1))
 
+		print(Temp)
+	except:
+		pass
+
+
+	if (Temp):
+		app.add_value(t,Temp)
+	t = t+1
+	if (t >= app.graph.rangex[1]):
+		app.graph.rangex = (app.graph.rangex[0],t+5)
+		app.graph.draw_background()
+		app.graph.draw()
+
+	root.after(1,handle_serial)
+
+
+
+
+
+
+
+#f = open("test.dat")
+#i = 1
+#for line in f.readlines():
+#	x = i
+#	y = round(float(line))
+#	app.add_value(i, y)
+#	i = i + 1
+#
+root.after(0,handle_serial)
 root.mainloop()
 
 
